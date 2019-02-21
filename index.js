3b0d9a524399b3408296908f0535cbdc00b80122
Kyle Reilly <reilly.kyle101@gmail.com>
2019-02-20 20:29:39 -0800

    handlebars activity

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-express = require('express')
+var express = require('express');
 
-app = express()
+var app = express();
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 
-app.set('port',3000)
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
 
-// Landing page route
-app.get('/',function(req,res){
-    res.type('text/plain');
-    res.send('Welcome to the main page!');
-})
+app.get('/', function (req, res) {
+    num = randomNum()
+    res.render('home',{title:"Home Page",randNum:num}) 
+});
 
-app.get('/other-page',function(req,res){
-    res.type('text/plain');
-    res.send('Welcome to the other page!')
-})
+app.get('/other-page', function (req, res) {
+    res.render('other-page',{title:'Another Page'});
+});
 
-// Error code processing
-app.use(function(req,res){
-    res.type('text/plain')
+app.use(function (req, res) {
     res.status(404);
-    res.send('404 - Not Found')
-})
+    res.render('404',{title:'404 - Page not Found'});
+});
 
-app.use(function(err, req, res, next){
-    console.error(err.stack)
-    res.type('plain/text')
-    res.status(500)
-    res.send('500 - Server Error')
-})
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
 
-app.listen(app.get('port'), function(){
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.')
-})
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+function randomNum() {
+    return Math.floor(Math.random()*10000+1)
+}
